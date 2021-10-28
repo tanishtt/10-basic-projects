7728b33c1a173d64e9f945331dc633600233be4b
TANISH MOHANTA <tanishmohanta1901@gmail.com>
2021-10-28 13:04:40 +0530

    added decrement function

--- project-1/script..js
+++ project-1/script..js
@@ -7,6 +7,11 @@ document.querySelector(".btn-plus").addEventListener("click",function(){
 count=document.querySelector("#counter").value;
 count++;
 document.querySelector("#counter").value=count;
+if(count>0)
+{
+    document.querySelector(".btn-minus").removeAttribute("disabled");
+    document.querySelector(".btn-minus").classList.remove("disabled");
+}
 });
 
 
@@ -15,4 +20,18 @@ document.querySelector("#counter").value=count;
 document.querySelector(".reset-to-zero").addEventListener("click",function(){
 count=0;
 document.querySelector("#counter").value=count;
+document.querySelector(".btn-minus").setAttribute("disabled","disabled");
+});
+
+
+//document.querySelector(".btn-minus").setAttribute("disabled","disabled");
+//decrement
+document.querySelector(".btn-minus").addEventListener("click",function(){
+count=document.querySelector("#counter").value;
+count--;
+document.querySelector("#counter").value=count;
+if(count==0)
+{
+    document.querySelector(".btn-minus").setAttribute("disabled","disabled");
+}
 });
