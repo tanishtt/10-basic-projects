e0018d20b9b21e7324ceb39761f6f0c340db6c5a
TANISH MOHANTA <tanishmohanta1901@gmail.com>
2021-12-17 10:41:57 +0530

    added function

---
+++ project-5-todolist-using-apis/script.js
@@ -0,0 +1,31 @@
+const tasks=[];
+const tasklist=document.getElementById('list');
+const addtaskinput=document.getElementById('add');
+const taskcounter=document.getElementById('tasks-counter');
+
+
+
+function deletetask(taskid)
+{
+
+}
+function marktaskascomplete(taskid)
+{
+
+}
+function rendertask()
+{
+
+}
+function addtask(task)
+{
+
+}
+function handleinputkeypress()
+{
+    
+}
+function shownotification(text)
+{
+
+}
